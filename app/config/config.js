342b42cc1c9d9686618619ac456b7650b1be6f78
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-10-12 13:59:07 +0200

    Suppress annoying warning

--- app/config/config.js
+++ app/config/config.js
@@ -2,6 +2,6 @@ import objectAssign from 'react/lib/Object.assign';
 import baseConfig from './all.js';
 
 // let config = require('webpack-config-loader!../../config/config.js');
-let config = require('../../config/config');
+const config = require('../../config/config');
 
 export default objectAssign(baseConfig, config);
